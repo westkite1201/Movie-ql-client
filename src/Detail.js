d84a0d3ddf1b6eee264379628fdc9b7fd3229312
nowindesire <nowindesire@gmail.com>
2019-07-29 18:00:27 +0900

    Movieql lecture 6

---
+++ src/Detail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Query} from "react-apollo";
+import { MOVIE_DETAILS } from './queries'
+const Detail = ({
+  match: {
+    params: { movieId }
+  }
+}) => <Query query={MOVIE_DETAILS} variables ={{movieId}} > 
+
+{({ loading, data, error }) =>{
+    if(loading) return "loading";
+    if(error) return "error";
+    return(
+        <React.Fragment>
+            {data.movie.title}
+            {data.movie.medium_cover_image}
+            {data.movie.rating}
+            {data.movie.description_intro}
+        </React.Fragment>
+    )
+}}
+</Query>
+
+export default Detail;
